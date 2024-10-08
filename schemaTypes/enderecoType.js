79b7f231f07c73b890ab00424dac86c26e0fdf14
Izarut <eduardotpanata@gmail.com>
2024-10-08 20:12:25 -0300

    up

---
+++ schemaTypes/enderecoType.js
@@ -0,0 +1,44 @@
+import { defineType } from "sanity"
+
+export const pessoa = defineType({
+    name: 'endereco',
+    type: 'document',
+    title: 'endereco',
+    fields:[
+        defineField({
+            name:'rua',
+            title: 'rua',
+            type: 'string' 
+        }),
+        defineField({
+            name:'numero',
+            title: 'numero',
+            type: 'string' 
+        }),
+        defineField({
+            name:'bairro',
+            title: 'bairro',
+            type: 'string' 
+        }),
+        defineField({
+            name:'cidade',
+            title: 'cidade',
+            type: 'string' 
+        }),
+        defineField({
+            name:'estado',
+            title: 'estado',
+            type: 'string' 
+        }),
+        defineField({
+            name:'pais',
+            title: 'pais',
+            type: 'string' 
+        }),
+        defineField({
+            name:'cep',
+            title: 'cep',
+            type: 'string' 
+        })
+    ]
+})
---
+++ schemaTypes/pessoaType.js
@@ -0,0 +1,49 @@
+import { defineField, defineType } from "sanity";
+
+export const pessoa = defineType({
+    name: 'pessoa',
+    type: 'document',
+    title: 'pessoa',
+    fields:[
+        defineField({
+            name:'id',
+            title: 'id' ,
+            type: 'number' 
+        }),
+        defineField({
+            name:'nome',
+            title:'nome',
+            type:'string'
+        }),
+        defineField({
+            name:'dataNascimento',
+            title:'data de nascimento',
+            type:'date'
+        }),
+        defineField({
+            name:'email',
+            title:'email',
+            type:'string'
+        }),
+        defineField({
+            name:'telefone',
+            title:'telefone',
+            type:'string'
+        }),
+        defineField({
+            name:'imagemPerfil',
+            title:'imagem de perfil',
+            type:'image'
+        }),
+        defineField({
+            name:'ativo',
+            title:'ativo',
+            type:'boolean'
+        }),
+        defineField({
+            name:'endereco',
+            title: 'endereco',
+            type: 'endereco' 
+        })
+    ]
+})
